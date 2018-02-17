0f5f743b5fe258363dc007289b8ea14dbd35b417
ChristophP <deedop@hotmail.de>
2018-02-17 01:06:45 +0100

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// see: https://github.com/webpack/webpack/issues/2537
+const isProd = process.argv.indexOf('-p') !== -1;
+const elmLoaderDefaults = { cwd: __dirname };
+
+module.exports = {
+  entry: {
+    bundle: path.resolve(__dirname, './src/Main.elm'),
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    library: 'Elm',
+  },
+  context: __dirname,
+  module: {
+    rules: [
+      {
+        test: /\.elm$/,
+        exclude: [
+          path.resolve(__dirname, 'elm-stuff'),
+          path.resolve(__dirname, 'node_modules'),
+        ],
+        use: {
+          loader: 'elm-webpack-loader',
+          options: isProd
+            ? elmLoaderDefaults
+            : {
+                ...elmLoaderDefaults,
+                debug: true,
+                warn: true,
+              },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Preview',
+      template: path.resolve(__dirname, 'src/index.html.ejs'),
+      filename: 'index.html',
+      inject: 'head',
+      minify: {
+        html5: true,
+        collapseWhitespace: true,
+        removeComments: true,
+        minifyJS: true,
+        minifyCSS: true,
+      },
+      inlineSource: '.(js|css)$', // embed all javascript and css inline
+    }),
+  ],
+  devServer: {
+    stats: 'errors-only',
+  },
+};
